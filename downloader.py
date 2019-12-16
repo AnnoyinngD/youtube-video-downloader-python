caa9e6c3b18c8cc9a20f9ea2905663c9e48f7bfd
DANIEL <daniel.asnake10001@gmail.com>
2019-12-16 11:54:13 -0800

    Update downloader.py

--- downloader.py
+++ downloader.py
@@ -1,35 +1,7 @@
-# from __future__ import unicode_literals
-# import youtube_dl
-# import os
-# from sys import argv
 
-# # Download data and config
-
-# download_options = {
-# 	'format': 'bestaudio/best',
-# 	'outtmpl': '%(title)s.%(ext)s',
-# 	'nocheckcertificate': True,
-# 	'postprocessors': [{
-# 		'key': 'FFmpegExtractAudio',
-# 		'preferredcodec': 'mp3',
-# 		'preferredquality': '192',
-# 	}],
-# }
-
-# # Song Directory
-# if not os.path.exists('Songs'):
-# 	os.mkdir('Songs')
-# else:
-# 	os.chdir('Songs')
-
-# # Download Songs
-# with youtube_dl.YoutubeDL(download_options) as dl:
-# 	with open('../' + argv[1], 'r') as f:
-# 		for song_url in f:
-# 			dl.download([song_url])
 
 from pytube import YouTube
-SAVE_PATH = "C:\Users\mini\Downloads\github2bu\Songs\" :
+SAVE_PATH = "folder locayion" :
 links = open('Songs.txt','r')
 for l in links:
 	get_true = True
